0aa8d97fc62b7957c4bd08ec467ecda9047a6ff0
VedP2809 <VP83@myscc.ca>
2022-03-06 23:14:44 -0500

    This is my assignment 5

---
+++ WEB303As05/script.js
@@ -0,0 +1,20 @@
+/*
+    Assignment 05
+*/
+
+$(document).ready(function () {
+    class ContentItem {
+        constructor(id, name, description, genre) {
+          this.id = id;
+          this.name = name;
+          this.description = description;
+          this.genre = genre;
+        }
+        updateContentItem() {
+            
+        }
+      }
+
+});
+
+
