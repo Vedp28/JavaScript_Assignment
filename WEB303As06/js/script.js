b55aa72171e5e9a8a6adeaf4d2bba4393b828aa0
VedP2809 <VP83@myscc.ca>
2022-03-13 22:12:24 -0400

    This is My new Assignment 06

--- WEB303As06/js/script.js
+++ WEB303As06/js/script.js
@@ -2,10 +2,32 @@ $(function () {
     $('.accordion .accordion-control').on('click', function (e) {
         e.preventDefault();
 
-        $(this)
-            .next('.accordion-panel')
-            .not(':animated')
-            // .stop()
+        let a = $(this).next('.accordion-panel');
+        $('.accordion-panel').not(a)
+            .slideUp();
+
+            a.not(':animated')
             .slideToggle();
+            
+
     });
+    $(".tab-list").each(function () {
+        let $this = $(this); 
+        let $tab = $this.find("li.active");
+        let $link = $tab.find("a");
+        let $panel = $($link.attr("href"));
+        $this.on("click", ".tab-control", function (e) {
+         
+          e.preventDefault();
+          let $link = $(this); 
+          let id = this.hash;
+          if (id && !$link.parent().is(".active")) {
+            // If not active
+            $panel.removeClass("active");
+            $tab.removeClass("active");
+            $panel = $(id).addClass("active");
+            $tab = $link.parent().addClass("active"); 
+          }
+        });
+      });
 });
