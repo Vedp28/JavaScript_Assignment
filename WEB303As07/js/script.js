f812f3083b349abadd6742abef03d95e8e7b393f
VedP2809 <VP83@myscc.ca>
2022-03-27 23:21:05 -0400

    This is my completed assignment 07

--- WEB303As07/js/script.js
+++ WEB303As07/js/script.js
@@ -3,9 +3,27 @@ $(function () {
         var $content = $(this).clone().find('img').css({
             marginLeft: 0,
             marginTop: 0,
-            width: '100%',
-            height: 'auto'
+            width: '800px',
+            height: '450px',
+            opacity: 100
         });
+
+
         //modal code goes here
-        });
+        e.preventDefault();
+        (function (someParameter) {
+            console.log(someParameter);
+            // Remove modal content from page and store in $content
+            var $content_ = $content.detach();
+            var modal = new Modal();
+            // Click handler calls open() method of modal object
+            
+            modal.open({
+                content: $content_,
+                width: 800,
+                height: 450,
+            });
+        })("ValuePassedIn");
+        
+    });
 });
