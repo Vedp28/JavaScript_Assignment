18e3e6f3e9410ee965621dd4597cd19b53b0a649
VedP2809 <VP83@myscc.ca>
2022-04-10 19:14:26 -0400

    This is my partially completed assignment 09

---
+++ WEB303As08/js/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    $('#search').keyup(function(){
+        $('#result').html('');
+        let a = $('#search').val();
+        let b = new RegExp(search, "i");
+        $.getJSON('index.json', function(data){
+            $.each(data, function(_key, value){
+                if(value.fname.search(b) != -1)
+                {
+                    $('#result').append('<li class="list-item"> '+value.fname+' '+value.lname+' '+value.played+' '+value.age+' '+value.income+' '+value.DOB+'')
+                }
+            });
+        })
+    })
+    $.getJSON("index.json", function(data){
+        let index_data = '';
+        $.each(data, function(key, value){
+            index_data += '<tr>';
+            index_data += '<td>'+value.fname+'</td>';
+            index_data += '<td>'+value.lname+'</td>';
+            index_data += '<td>'+value.played+'</td>';
+            index_data += '<td>'+value.age+'</td>';
+            index_data += '<td>'+value.income+'</td>';
+            index_data += '<td>'+value.DOB+'</td>';
+
+        });
+        $('#table').append(index_data);
+    });
+
+    $("#but1").click(function(e){
+        $("#table tr:not(:first-child)").filter(function (e) {
+            let x = $(this).find("td:eq(1)").text();
+            
+                if (x.includes("A") || x.includes("B") || x.includes("C") || x.includes("D") || x.includes("E") || x.includes("F") || x.includes("G") || x.includes("H") || x.includes("I") || x.includes("J") || x.includes("K") || x.includes("L") || x.includes("M")) {
+                    $(this).show();
+                }
+                else
+                {
+                    $(this).hide();
+                }
+                y = $('tr:visible').length;
+
+                $('#button1').text(`( ${y - 1} )`);
+                $('#button2').text("");
+        });
+    });
+    $('th').click(function(){
+        let table = $(this).parents('table').eq(0)
+        let rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+        this.asc = !this.asc
+        if (!this.asc){rows = rows.reverse()}
+        for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+    })
+    function comparer(index) {
+        return function(a, b) {
+            var valA = getCellValue(a, index), valB = getCellValue(b, index)
+            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+        }
+    }
+    function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
+        
+    $("#but2").click(function(e){ 
+        $("#table tr:not(:first-child)").filter(function (e) {
+            var x = $(this).find("td:eq(1)").text();
+            
+            if (x.includes("O") || x.includes("P") || x.includes("Q") || x.includes("R") || x.includes("S") || x.includes("T") || x.includes("U") || x.includes("V") || x.includes("W") || x.includes("X") || x.includes("Y") || x.includes("Z")) {
+                $(this).show();
+            }
+            else
+            {
+                $(this).hide();
+            }
+            y = $('tr:visible').length;
+
+            $('#button2').text(`( ${y - 1} )`);
+            $('#button1').text("");
+        });
+    });
+});
